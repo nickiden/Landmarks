068becbc0c7b4a78992b52465215b689831ed679
Nick Iden <iden.nick@gmail.com>
2019-12-31 12:45:42 -0500

    Added page elements circle image and map to a single Vstack

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Nick Iden on 2019-12-31.
+//  Copyright © 2019 Nick Iden. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    // create a function that sets proper location
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
